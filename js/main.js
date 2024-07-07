0ccace356eb9021e6e4aae022f2d436a5db9a665
Evgeniy Pavlichenko <92581680+JeK566787@users.noreply.github.com>
2024-07-07 14:54:15 +0300

    add assortment & change menu__btn to menu__burger

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const menuBtn = document.querySelector('.menu__btn');
+const menuBtn = document.querySelector('.menu__burger');
 const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.menu__list');
 const backDrop = document.querySelector('.backdrop--close');
